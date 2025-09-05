5380b2c19010e20de002522cbc6621f4ce27a337
USER <USER@DESKTOP-UOI0DH3>
2025-09-05 20:47:50 +0530

    Updated push

--- src/test/java/testcasesnew/GitPushFromOriginalWorkspace.java
+++ src/test/java/testcasesnew/GitPushFromOriginalWorkspace.java
@@ -4,7 +4,7 @@ public class GitPushFromOriginalWorkspace {
 
 	public static void main(String[] args) {
 		System.out.println("Git Push From Original Workspace");
-
+		System.out.println("Updated git push");
 	}
 
 }
